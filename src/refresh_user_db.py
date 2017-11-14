6220aced4c2168a113e637f22a3676b63b30fb40
Ubuntu <ubuntu@ip-172-31-24-198.eu-west-2.compute.internal>
2017-11-14 23:07:00 +0000

    Fix refresher

--- src/refresh_user_db.py
+++ src/refresh_user_db.py
@@ -3,10 +3,6 @@ from db import DataStore
 def refresh_user_db():
     db = DataStore({})
     reports = db.read_all()
-    for report in reports:
-        db.add_user(report['reporter'])
-        db.add_spammer(report['author'])
-    
     for user in db.get_all_user():
         db.update_point(user['user_id'])
 
